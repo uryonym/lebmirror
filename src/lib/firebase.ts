2e25024c2ad25b84f96d53b3e4db512e8f34b21f
yoneyama ryo <uryonym@outlook.com>
2024-02-04 00:34:40 +0900

    firebaseの基本設定を実装

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app'
+import { GoogleAuthProvider, getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+export const googleProvider = new GoogleAuthProvider()
+export const firebaseAuth = getAuth(firebaseApp)
+export const firebaseDb = getFirestore(firebaseApp)
